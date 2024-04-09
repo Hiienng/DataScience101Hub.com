94d69ed013375f8d345610f91ec1f16b717dc632
Hien Ng <89530538+Hiienng@users.noreply.github.com>
2024-04-09 14:18:46 +0700

    Delete ML_Banking.py

--- ML_Banking.py
+++
@@ -1,9 +0,0 @@
-class TEAMA:
-    member = 0 
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        TEAMA.member +=1
-    def get_yob(self):
-        return 2023 - self.age
